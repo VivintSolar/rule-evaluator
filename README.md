4bd7a8062339af77b1c756f0b301b8b75a8c73f3
kenneth chatfield <chatfield.kenny@gmail.com>
2017-10-25 18:09:43 -0600

    class base evaluation

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # rule-evaluator
-#####Node Package to Evaluate Rules for a Service
+##### Node Package to Evaluate Rules for a Service
 
 ## Installation
 
@@ -9,78 +9,244 @@ $ npm i --save rule-evaluator
 ```
 ## Usage
 
+### **Prerequisites**
+#### `Association Object`
+
+The `Association Object` contains all the information needed to evaluate rules for a service. This is also needed to initialize The `rule-evaluator`.
+
+* End-Point: ahj.vivintsolar.com/api/service/`:serviceNumber`/association
+
 ##### In Node.js:
 ```js
-// Load the rule-evaluator
 const ruleEvaluator = require('rule-evaluator');
-// Prepare Necessary Parameters
-const serviceData = getServiceData(),
-    inputObject = getInputObject();
+
+const associationObject = getAssociationObject();
     
-//Pass Necessary Parameters to the rule-evaluator and Recieve an Updated Service Object
-const evaluatedRules = ruleEvaluator(serviceData, inputObject);
+// initialize ruleEvaluator
+const evaluator = new ruleEvaluator(associationObject);
 ```
 
-## Parameters
-1) `serviceData`: Service Associated Data From the Requirements Manager
-2) `inputObject`: Client Data for Evaluation A.K.A options
+### **Rule Evaluator Class**
 
-### Service Data
-* [Requirements Manager](https://github.com/VivintSolar/ahj-scope)
+#### *Outline:*
 
-### Input Object
+#### `Getters`
+* `conditions`: `Map`, of all the current Conditions for evaluation
+* `rules`: `Array`, of all the current Rules to evaluate
+* `serviceAhj`: `Object`, returns the state of The Service Ahj based on previous evaluations
+* `appliedRules`: `Array`, list of all the Rules that are applied to that service
+* `appliedConditions`: `Array`, of all the Conditions that are applied to that service
+    * When `rules` have been set, returns only Conditions applied to rules found in that list
+* `appliedConditionsMap`: `Map`, of Rule IDs and an `Array` of the Conditions applied to them
+    * When `rules` have been set, returns only Conditions applied to rules found in that list
+* `errors`: `Array`, of any errors found while Setting or Evaluating
+* `errorsMap`: `Map`, of Ruled IDs with errors and an `Array` of any errors found while Setting or Evaluating
 
-* Data Type: Object/Map
-* Fields
-    * rules
-    * inputs
-    
-#### Rules
+#### `Setters`
+* `conditions`: `Map`, sets a map of conditions to use when evaluating
+* `rules`: `Array`, sets an array of rule ids to specify what rules to apply an evaluation on
+    * When `null`/`undefined` All Applied Rules will be evaluated
 
-* Data Type: Array
-* Field Name: "rules"
-* List of Predefined Rule id's to Evaluate
+#### Methods
+* `evaluate()`: Based on Rules and Conditions that were set, Applied Service Conditions and Input Conditions will be compared and values will be calculated based on those comparisons
+    * A Truly Successful Evaluation will have NO errors
+    * Results Found in path `serviceAhj.rules`
+* `exceptions()`: Will generate the Default Values for each Rule Specified, or all Rules if `rules` arn't set, and a description of all Applied Conditions and the values associated with them (A.K.A Exceptions)
+    * If no description found in rule then no Conditions were applied to that rule in that particular service
 
-Example:
+#### *Examples:*
 
+**Set Rules**
 ```js
-const inputObject = {
-    rules:['fireCodeSetbacks','roofDesignSetbacks'],
-    ...
+evaluator.rules = ["defPointSetbacks", "sunHourMinimums"]
+```
+**Get Applied Conditions**
+```js
+const appliedConditions = evaluator.appliedConditions
+console.log(appliedConditions)
+```
+*Example Log Statement*
+```js
+[
+    "contractType",
+    "roofType"
+]
+```
+**Set Conditions**
+```js
+evaluator.conditions = { 
+   contractType:"pace",
+   roofType:"tile"
 }
 ```
-**NOTE:** <br>
-If rules are left empty then all rules applied to the service associated ahj will be evaluated
 
-#### Inputs
-* Data Type: Object/Map
-* Field Name: "inputs"
-* Map Containing a list of Condition Properties and their associated values
-    * key: Predefined Condition Property id
-    * value: Valid Value, Meaning a value that satisfies and is found within the bounds of the Predefined Condition Property
-
-Example:
+**Evaluate**
+* Results Determined By Set `rules` and `conditions`
 
 ```js
-const inputObject = {
-    rules:['fireCodeSetbacks','roofDesignSetbacks'],
-    inputs:{
-            roofType:"tile",
-            structureType:"townHouse",
-            roofPlaneLocation:"garage",
-            adjacentRidgeRoofPlanePercentOccupied:0,
-            overlappingRoofHeightDistance:0,
-            firecodeAlternativeMiddlePathDistance:0,
-            adjacentRidgeSetbackDistance:36
+const serviceAhj = evaluator.evaluate();
+console.log(serviceAhj);
+// OR: same thing, different way
+evaluator.evaluate();
+console.log(evaluator.serviceAhj);
+```
+*Results*
+```json
+{
+    "latitude": 33.8413,
+    "longitude": -117.925,
+    "service_number": "...",
+    "office_id": "4017",
+    "Office": "CA-10 Orange County Solar",
+    "customer_name": "...",
+    "street": "748 N RESH ST",
+    "Utility": "APU",
+    "City": "Anaheim",
+    "County": "Orange County",
+    "State": "California",
+    "name": ":service_number",
+    "type": "service",
+    "ahjRefs": [
+        {
+            "id": "32990245-6c76-40d5-9d64-e57fd2d39025",
+            "name": "CA-10 Orange County Solar",
+            "type": "Office"
+        },
+        {
+            "id": "9d79e761-6445-4098-9a00-4e9f88797533",
+            "name": "APU",
+            "type": "Utility"
+        },
+        {
+            "id": "cd1855ff-8796-4689-bdb6-ad5396d83f4d",
+            "name": "Anaheim",
+            "type": "City"
         }
+    ],
+    "rules": {
+        "sunHourMinimums": {
+            "description": "When Contract Type is PACE; System:1300, Module:950",
+            "conditions": [
+                {
+                    "left": "contractType",
+                    "operator": "=",
+                    "right": "pace"
+                }
+            ],
+            "value": {
+                "system": 1300,
+                "module": 950
+            },
+            "source": {
+                "id": "9d79e761-6445-4098-9a00-4e9f88797533",
+                "name": "APU",
+                "type": "Utility"
+            }
+        },
+        "defPointSetbacks": {
+            "description": "If The Roof Type is Tile",
+            "conditions": [
+                {
+                    "left": "roofType",
+                    "operator": "=",
+                    "right": "tile"
+                }
+            ],
+            "value": {
+                "serviceLine": 36,
+                "mechanicalUnit": 36,
+                "side": 18,
+                "valley": 18,
+                "ridge": 36,
+                "eave": 18,
+                "obstructions": 12,
+                "hip": 18
+            },
+            "source": {
+                "id": "cd1855ff-8796-4689-bdb6-ad5396d83f4d",
+                "name": "Anaheim",
+                "type": "City"
+            }
+        }
+    }
 }
 ```
-**NOTE:** <br>
-If inputs are left empty then a "simple evaluation" path will be taken thus returning only default values
+#### __*Results, Explanation*__
 
+* `latitude`: `String`, Latitude Of Service From SalesForce,
+* `longitude`: `Float`, Longitude Of Service From SalesForce,
+* `service_number`: `String`, Service Number of this instance,
+* `office_id`: `String`, Field Office ID From SalesForce,
+* `Office`: `String`, Name of Field Office From SalesForce,
+* `customer_name`: `String`, Customer Name From SalesForce,
+* `street`: `String`, Street Address of service From SalesForce,
+* `Utility`: `String`, Utility Company Name From SalesForce,
+* `City`: `String`, City Name From GeoLocation,
+* `County`: `String`, County Name From GeoLocation,
+* `State`: `String`, State Name From GeoLocation,
+* `name`: `String`, Service Number of this instance,
+* `type`: `String`, Type Of AHJ`:service(because its an aggregation of all the types),
+* `ahjRefs`: `Array`, reference to documents used to create this Service Ahj
+    * `id`: `String`, AHJ Document ID,
+    * `name`: `String`, AHJ Name,
+    * `type`: `String`, AHJ Type
+* `rules`: `Map`, of Evaluated Rules
+    * `sunHourMinimums`: `Map`, of Evaluated Applied Rule Data
+        * `description`: `String`, Text Description of Condition that Resulted in the below value,
+        * `conditions`: `Array`, Applied Conditions That Evaluated to True, If Conditions Field is Absent then the value was determined by a default
+            * `left`: `String`, Condition ID,
+            * `operator`: `String`, Operator Used in Comparision/Calculation,
+            * `right`: `Condition Defined Data-Type`, Predefined Condition Value That Evaluated to be True when compared to Input Condition Value
+        * `value`: `Rule Defined Data-Type`, Format of Value is Determined by the Data-Type and attributes Defined in the Rule Definition
+            * `system`: `Rule Defined Data-Type`, value that evaluated to true,
+            * `module`: `Rule Defined Data-Type`, value that evaluated to true
+        * `source`: `Map`, Reference to ahj where these values came from 
+            * `id`: `String`, AHJ Document ID,
+            * `name`: `String`, AHJ Name,
+            * `type`: `String`, AHJ Type
+            
+### __*Handling Errors*__
+__*Error Types*__
+* `invalidCondition`: When Set Condition is Not a Predefined or Applied Condition
+* `invalidRule`: When Set Rule is Not a Predefined or Applied Rule
+* `missingCondition`: When an Applied Rule has Applied Conditions and a Condition Input Mapping as not made for it
 
+#### __*Error Causing Statement, Examples*__
+**Invalid Rule**
+```js
+evaluator.rules = ["invalidRule","defPoints"];
+```
+**Invalid Condition, Missing Condition**
+```js
+evaluator.conditions = { 
+   invalidCondition:22
+}
+console.log(evaluator.errors)
+```
+*Errors*
+```json
+[
+    {
+        "id": "invalidRule",
+        "message": "Invalid Rule Provided, Rule Not Found: invalidRule",
+        "timeStamp": "2017-10-25T23:49:05.384Z",
+        "type": "invalidRule"
+    },
+    {
+        "id": "invalidCondition",
+        "message": "Invalid Condition Provided, Condition Not Found: invalidCondition",
+        "timeStamp": "2017-10-25T23:52:00.539Z",
+        "type": "invalidCondition"
+    },
+    {
+        "id": "roofType",
+        "message": "Condition Input Missing: roofType",
+        "timeStamp": "2017-10-25T23:52:00.539Z",
+        "type": "missingCondition"
+    }
+]
+```
 
-
-
+        
 
 
--- index.js
+++ index.js
@@ -1,11 +1,272 @@
-const evaluate = require('./evaluation/evaluate').evaluate;
+module.exports = class Evaluation {
+    constructor({
+        serviceAhj,
+        documents,
+        definitions,
+        conditions,
+        rules
+    }) {
+        this.mergeRules = () => {
+            let appliedRules = {};
+            this._documents.map(ahj=>{
+                const rules = this._rules||Object.keys(ahj.rules);
+                const {id, name, type} = ahj;
+                rules.map(key=>{
+                    const rule = ahj.rules[key];
+                    if(rule){
+                        if(!appliedRules[key]){
+                            appliedRules[key] = Object.assign({},{id:key, statements:[]})
+                        }
+                        rule.statements.map(statement=>{
+                            appliedRules[key].statements.push(
+                                Object.assign({}, statement, {source:{id,name,type}})
+                            )
+                        })
+                    }
+                })
+            });
+            return appliedRules;
+        };
+        this._serviceAhj = serviceAhj;
+        this._documents = documents;
+        this._definitions = definitions;
+        this._conditions = conditions;
+        this._rules = rules;
+        this._appliedRules = this.mergeRules();
+        this._errors = null;
+        this._timeStamp = new Date();
+        this._evaluationType = undefined;
+    }
+    set conditions(conditions) {
+        this._conditions = conditions;
+        this.validateConditions()
+    }
+    clearConditions(){
+        this._conditions = undefined;
+        this.validateConditions()
+    }
+    clearRules(){
+        this._rules = undefined;
+        this.validateRules();
+    }
+    get conditions(){
+        return this._conditions;
+    }
+    set rules(rules) {
+        this._rules = rules;
+        this.validateRules();
+    }
+    get validatedRules(){
+        const appliedRules = Object.keys(this._appliedRules);
+        if(this._rules) return this._rules.filter(id=>{return appliedRules.includes(id) })
+    }
+    get rules(){
+        return this._rules;
+    }
+    get serviceAhj(){
+        return this._serviceAhj;
+    }
+    get appliedRulesDetail(){
+        return this._appliedRules;
+    }
+    get appliedRules(){
+        return Object.keys(this._appliedRules);
+    }
+    get appliedConditionsMap(){
+        const rules = this.validatedRules||Object.keys(this._definitions.conditions);
+        return Object.assign({},
+            ...rules.map(id=>{
+                const appliedConditions = this._appliedConditions(id);
+                if(appliedConditions) return {[id]:appliedConditions};
+                return {};
+            })
+        )
+    }
+    get appliedConditions(){
+        if(!this.validatedRules) return Object.keys(this._definitions.conditions);
+        return this.flatten(this.validatedRules.map(id=>{
+            return this._appliedConditions(id)
+        }));
+    }
+    get errorsMap(){
+        return (this._errors)?this._errors.map:this._errors;
+    }
+    get errors(){
+        return (this._errors)?this._errors.list:this._errors;
+    }
+    get exceptions(){
+        this._evaluationType = 'exceptions';
+        this.validateRules();
+        this.parseRules();
+        return this._serviceAhj;
+    }
+    get evaluate(){
+        this._evaluationType = 'evaluate';
+        this.validateConditions();
+        this.validateRules();
+        this.parseRules();
+        return this._serviceAhj;
+    }
+    flatten(array){
+        let flat = [];
+        array.map(a=>{if(a) flat = [...flat,...a]});
+        return flat;
+    }
+    _appliedConditions(id){
+        let appliedConditions;
+        this._appliedRules[id].statements.map(statement=>{
+            if(statement.condition){
+                if(!appliedConditions) appliedConditions = [];
+                statement.condition.map(condition=>{
+                    if(!appliedConditions.includes(condition.left)) appliedConditions.push(condition.left)
+                })
+            }
+        });
+        return appliedConditions;
+    }
+    cleanUpErrors(ids){
+        if(ids){
+            ids.map(id=>{
+                if( this._errors && this._errors.map[id] ){
+                    this._errors.map[id].filter(error=>{return error.timeStamp === this._timeStamp});
+                    this._errors.list.map(error=>{return error.id!==id||error.timeStamp === this._timeStamp});
+                    if(this._errors.map[id].length === 0) this._errors.map[id] = undefined;
+                    if(this._errors.list.length === 0 ) this._errors = undefined;
+                }
+            })
+        }
+    }
+    addError(id,message,type){
+        if(message==='clear') this._errors = null;
+        else {
+            const timeStamp = this._timeStamp;
+            if(!this._errors) this._errors = {list:[],map:{}};
+            if(!this._errors.map[id]) this._errors.map[id] = [];
+            this._errors.map[id].push({id,message,timeStamp,type});
+            this._errors.list.push({id,message,timeStamp,type});
+        }
+    }
+    validateRules(){
+        this._timeStamp = new Date();
+        this.cleanUpErrors(this._rules);
+        if(this._rules){
+            this._rules.map(id=>{
+                if(!this._definitions.listAll.rules.includes(id)){
+                    this.addError(id,`Invalid Rule Provided, Rule Not Found: ${ id }`,'invalidRule')
+                }
+            })
+        }
+    }
+    validateConditions(){
+        this._timeStamp = new Date();
+        const appliedConditions = this.appliedConditions;
+        const conditionInputs = Object.keys(this._conditions||{});
+        this.cleanUpErrors([...appliedConditions,...conditionInputs]);
+        conditionInputs.map(id=>{
+            if(!this._definitions.listAll.conditions.includes(id)){
+                this.addError(id,`Invalid Condition Provided, Condition Not Found: ${ id }`,'invalidCondition')
+            }
+        });
+        appliedConditions.map(appliedCondition=>{
+            if(!conditionInputs.includes(appliedCondition)){
+                this.addError(appliedCondition,`Condition Input Missing: ${ appliedCondition }`,'missingCondition');
+            }
+        })
+    }
+    calculator (input,operator,curRight) {
+        const calculate = {
+            "=":(input,curRight)=>{return input === curRight},
+            ">":(input,curRight)=>{return input > curRight},
+            "<":(input,curRight)=>{return input < curRight},
+            "!=":(input,curRight)=>{return input !== curRight},
+            ">=":(input,curRight)=>{return input >= curRight},
+            "<=":(input,curRight)=>{return input <= curRight}
+        };
+        return calculate[operator](input,curRight);
+    };
+    evaluateConditions(conditions){
+        if(!conditions) return true;
+        if(!this._conditions) return false;
+        const evaluation = conditions.map(condition=>{
+            const { left, operator, right} = condition;
+            const input = this._conditions[left];
+            return this.calculator(input,operator,right);
+        });
+        return !evaluation.includes(false)
+    }
+    objectDescriptions(statements,descriptions){
+        statements.map(statement=>{
+            if(statement.description){
+                if(!descriptions) descriptions = {};
+                Object.keys(statement.value).map(key=>{
+                    let description = statement.description;
+                    if(!descriptions[key])descriptions[key]=[];
+                    if(description.indexOf(statement.value[key])<0)description = `${key.toUpperCase()}: ${statement.value[key]}, ${description}`;
+                    descriptions[key].push(description);
+                });
+            }
+        });
+        return descriptions
+    }
+    mergeStatements(id,statements){
+        let obj, val, conditions, description, source;
+        const { dataType } = this._definitions.rules[id].template;
+        if(dataType==='object') obj = {};
+        statements.map(statement=>{
+            const { value, condition } = statement;
+            if(condition)conditions = [...conditions||[], ...condition];
+            if(obj) Object.assign(obj, value);
+            description = statement.description;
+            val = value;
+            source = statement.source;
+        });
+        return Object.assign(
+            description?{description}:{},
+            conditions?{conditions}:{},
+            {value: obj||val},
+            { source }
+        );
+    }
+    resolveConflicts(id, statements){
+        const standard = () => {
+            let theType, types = ['City','County','Utility','Office','State',"Nation"];
+            return statements.sort(function(a,b){
+                return types.indexOf(a.source.type) < types.indexOf(b.source.type) ? -1 : 1;
+            }).filter((statement,index) => {
+                if(index===0) theType = statement.source.type;
+                return statement.source.type === theType;
+            });
+        };
+        const ruleDefinition = this._definitions.rules[id];
+        if(ruleDefinition.template.onConflict === 'standard'){
+            return standard();
+        }
+        return statements;
+    }
+    parseStatements(id, statements){
+        let descriptions;
+        const { dataType } = this._definitions.rules[id].template;
+        const conflictFreeStatements = this.resolveConflicts(id,statements);
+        const evaluated = conflictFreeStatements.filter(statement=>{
+            if(this._evaluationType==='exceptions'&&statement.description){
+                if(!descriptions) descriptions = [];
+                descriptions.push(statement.description);
+            }
+            return this.evaluateConditions(statement.condition)
+        });
+        if(dataType==='object'&&this._evaluationType==='exceptions'){
+            descriptions = this.objectDescriptions(conflictFreeStatements)
+        }
+        return Object.assign(descriptions?{ descriptions }:{}, this.mergeStatements(id,evaluated))
 
-/**
- * Evaluates a set of Rules
- * @param {Object[]} data
- * @param {Object} inputObject
- * @return {string}
- */
-module.exports = function(data, inputObject) {
-    return evaluate(data, inputObject);
+    }
+    parseRules(){
+        const rules = this.validatedRules||Object.keys(this._appliedRules);
+        rules.map(id=>{
+            if(id){
+                const appliedRule = this._appliedRules[id];
+                this._serviceAhj.rules[id] = this.parseStatements(id,appliedRule.statements)
+            }
+        });
+    }
 };
