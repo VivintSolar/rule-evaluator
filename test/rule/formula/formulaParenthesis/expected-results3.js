06459db608f62e81bc7d8c58fae426f0a15e3a69
kenneth chatfield <chatfield.kenny@gmail.com>
2019-06-14 19:14:27 -0600

    order of operations for formulas

--- test/rule/formula/formulaParenthesis/expected-results3.js
+++ test/rule/formula/formulaParenthesis/expected-results3.js
@@ -2,11 +2,12 @@
 
 
 module.exports = JSON.stringify(
-    {
-        formula: "",
-        exceptions: null,
-        conditions: null,
-        value: 1,
-        errors: null
-    }
+    { formula:
+        '((10000 * 0.547) + ((dcPtcSystemSize * 1000) - 10000) * 0.473) * (designFactor / 100) * systemAdjustmentFactor',
+            exceptions: null,
+            conditions: [ { left: 'designFactor', operator: '<', right: 75 } ],
+            source: undefined,
+            description: 'Condition 16',
+            value: 2883.5052582384,
+            errors: null }
 );
--- test/rule/formula/formulaParenthesis/index.js
+++ test/rule/formula/formulaParenthesis/index.js
@@ -9,47 +9,45 @@ const assocObj = require('./associationObject');
 module.exports = () => {
     const evaluator = new Evaluator( assocObj );
 
-    // let ruleId = 'epbbFormula';
-    //
-    // let ruleEvaluator = evaluator.getRule( ruleId );
-    //
-    // let conditions = {
-    //     systemSizeDc: 0,
-    //     designFactor: 55,
-    //     cecAcSystemSize: 6.653
-    // };
-    //
-    // let evaluated = ruleEvaluator.evaluate( conditions );
-    //
-    // assert.equal( expected, JSON.stringify(evaluated) );
-    // console.log('RULE --- Formula Parenthesis 1---> Success!!!!!!!!!');
-    //
-    // ruleId = 'epbbFormula2';
-    //
-    // ruleEvaluator = evaluator.getRule( ruleId );
-    //
-    // conditions = {
-    //     cecAcSystemSize: 4
-    // };
-    //
-    // evaluated = ruleEvaluator.evaluate( conditions );
-    //
-    // assert.equal( expected2, JSON.stringify(evaluated) );
-    // console.log('RULE --- Formula Parenthesis 2---> Success!!!!!!!!!');
-
-    let ruleId = 'epbbFormula3';
+    let ruleId = 'epbbFormula';
 
     let ruleEvaluator = evaluator.getRule( ruleId );
 
     let conditions = {
+        systemSizeDc: 0,
+        designFactor: 55,
+        cecAcSystemSize: 6.653
+    };
+
+    let evaluated = ruleEvaluator.evaluate( conditions );
+
+    assert.equal( expected, JSON.stringify(evaluated) );
+    console.log('RULE --- Formula Parenthesis 1---> Success!!!!!!!!!');
+
+    ruleId = 'epbbFormula2';
+
+    ruleEvaluator = evaluator.getRule( ruleId );
+
+    conditions = {
+        cecAcSystemSize: 4
+    };
+
+    evaluated = ruleEvaluator.evaluate( conditions );
+
+    assert.equal( expected2, JSON.stringify(evaluated) );
+    console.log('RULE --- Formula Parenthesis 2---> Success!!!!!!!!!');
+
+    ruleId = 'epbbFormula3';
+
+    ruleEvaluator = evaluator.getRule( ruleId );
+
+    conditions = {
         designFactor: 55.6,
         systemAdjustmentFactor: 0.846,
         dcPtcSystemSize: 11.3958
     };
 
-    let evaluated = ruleEvaluator.evaluate( conditions );
-    console.log(':::::::: evaluated ::::::::\n', evaluated);
-    console.log(':::::::: evaluated ::::::::');
+    evaluated = ruleEvaluator.evaluate( conditions );
     assert.equal( expected3, JSON.stringify(evaluated) );
     console.log('RULE --- Formula Parenthesis 3---> Success!!!!!!!!!');
 };
