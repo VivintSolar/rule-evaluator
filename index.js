1eb030eaa9708def82f11a1cd2dbe2742d6688f3
kenneth chatfield <chatfield.kenny@gmail.com>
2020-03-09 17:55:12 -0600

    map rule dependencies for reference

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const initialize = require('./lib/initialize');
 const executeTemplateString = require('./lib/execute/executeTemplateString');
 const executeFormula = require('./lib/execute/executeFormula');
 
-//VERSION: 1.3.3
+//VERSION: 1.3.4
 
 class RuleEvaluator {
     constructor( parameters ) {
