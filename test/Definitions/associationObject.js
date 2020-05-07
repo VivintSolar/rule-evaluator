c7110a3176be89cd5944c1aee8a7f630a93acac7
kenneth chatfield <chatfield.kenny@gmail.com>
2020-05-07 16:05:51 -0600

    definitions getters

--- test/Definitions/associationObject.js
+++ test/Definitions/associationObject.js
@@ -3,4 +3,11 @@ const Definitions = require('../../lib/Definitions');
 
 const definitions = new Definitions({});
 
-const numberCondition = new NumberCondition();
+const numberCondition = new NumberCondition({});
+
+numberCondition.setRange({
+    min: 0,
+    max: 100
+});
+
+definitions.addDefinition( numberCondition );
