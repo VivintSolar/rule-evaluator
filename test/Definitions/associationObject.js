a4b28c8864d83f1503e3915175022bce74fd2f9d
kenneth chatfield <chatfield.kenny@gmail.com>
2020-05-07 11:23:31 -0600

    evaluate set and appliedConditions

---
+++ test/Definitions/associationObject.js
@@ -0,0 +1,6 @@
+const NumberCondition = require('../utilities/definitions/conditions/numberCondition');
+const Definitions = require('../../lib/Definitions');
+
+const definitions = new Definitions({});
+
+const numberCondition = new NumberCondition();
